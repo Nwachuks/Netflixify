8e2cb2458a66cfdf31c8305e6fabe41e3d02a990
Nwachukwu <nwachukwu@cowrywise.com>
2022-05-24 09:27:59 +0100

    Initial setup

---
+++ Netflixify/NetflixifyUITests/NetflixifyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NetflixifyUITestsLaunchTests.swift
+//  NetflixifyUITests
+//
+//  Created by Nwachukwu Ejiofor on 24/05/2022.
+//
+
+import XCTest
+
+class NetflixifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
